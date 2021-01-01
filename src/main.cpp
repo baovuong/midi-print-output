06533f91d0253cdd4375d893d36a633802640269
Bao Vuong <vqbao@lv5.org>
2021-01-01 18:46:06 -0500

    able to print out midi inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <cstdlib>
 #include <rtmidi/RtMidi.h>
-
-using namespace std;
 
 int main(int argc, char *argv[]) {
 
+    RtMidiIn *midiIn;
+
     try {
-        RtMidiIn midin;
-        cout << "Hello!" << endl;
+        midiIn = new RtMidiIn();
 
     } catch (RtMidiError &error) {
         error.printMessage();
+        exit( EXIT_FAILURE );
     }
+
+    unsigned int nPorts = midiIn->getPortCount();
+    std::cout << std::endl << "There are " << nPorts << " MIDI input sources available." << std::endl;
+    std::string portName;
+
+    // iterate through ports 
+    for (int i = 0; i < nPorts; i++) {
+        try {
+            portName = midiIn->getPortName(i);
+        } catch (RtMidiError &error) {
+            error.printMessage();
+            goto cleanup;
+        }
+
+        std::cout << "input " << i+1 << ": " << portName << std::endl;
+    }
+
+      // Clean up
+    cleanup:
+    delete midiIn;
     
     return 0;
 }
